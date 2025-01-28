68419b0dd28d1a3948868afe3bd4371041d19248
DaiyaanMuhammadFardeen <daiyaan.muhammad2002@gmail.com>
2025-01-28 17:00:28 +0600

    First working commit

---
+++ src/main/java/com/coductivity/Coductivity/CoductivityApplication.java
@@ -0,0 +1,13 @@
+package com.coductivity.Coductivity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoductivityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoductivityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coductivity/Coductivity/CoductivityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coductivity.Coductivity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoductivityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
